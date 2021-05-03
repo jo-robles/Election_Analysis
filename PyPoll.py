e57cb42f31ffea7440ba0908f559392ed8168b04
jo-robles <82462368+jo-robles@users.noreply.github.com>
2021-05-02 20:28:31 -0500

    Adding updated files

--- PyPoll.py
+++ PyPoll.py
@@ -1,22 +1,15 @@
-#Initalizing modules
+#Adding our dependencies.
 import csv
 import os
 
-#Assign a variable for the file to load and the path.
-file_to_load=os.path.join('Resources','election_results.csv')
+#Assign a variable to load a file from a path
+file_to_load = os.path.join('Resources','election_results.csv')
 
-#Open the election results and read the file
+#Assign a variable to save the file to a path.
+file_to_save = os.path.join('analysis','election_analysis.txt')
+
+#Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    #Print the file object
-    print(election_data)
-
-#close the file
-election_data.close()
-
-
-#1. The total number of votes cast
-#2. A complete list of candidates who received votes
-#3. The percentage of votes each candidate won
-#4. The total number of votes each candidate won
-#5. The winner of the election based on popular votes.
+    
+    #To do: read and analyze the data here. 
+    
--- Python_practice.py
+++ Python_practice.py
@@ -1,3 +1,9 @@
-testfile = 'readme.md'
-test = open(testfile,'r')
-print(test)
+import os
+import csv
+#cwd = os.getcwd()
+#print("My current working directory is: {} ".format(cwd))
+
+file_to_save = os.path.join('analysis','election_analysis.txt')
+with open(file_to_save, 'w') as txt_file:
+    txt_file.write('Hello World')
+    
