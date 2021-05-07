8be1e6994ecd7f4fb58921fb727abf17130f8ffe
jo-robles <82462368+jo-robles@users.noreply.github.com>
2021-05-06 21:52:29 -0500

    Adding updated codes and files

--- PyPoll.py
+++ PyPoll.py
@@ -1,89 +1,85 @@
-#Adding our dependencies.
+#Load dependencies
 import csv
 import os
 
-#Assign a variable to load a file from a path
+#Assign a variable to load a file from a path.
 file_to_load = os.path.join('Resources','election_results.csv')
 
 #Assign a variable to save the file to a path.
-file_to_save = os.path.join('analysis','election_analysis.txt')
+file_to_save = os.path.join('Analysis', 'election_analysis.txt')
 
-#Initialize a total vote counter.
+#Set total_votes to 0
 total_votes = 0
 
-#Creating a new list of the candidate options
+#Create a list of candidates.
 candidate_options = []
 
-#Declare the empty dictionary
+#Create an empty dictionary.
 candidate_votes = {}
 
-#Winning Candidate and Winning Count Tracker
-winning_candidate = ""
+#Create the variables for the winning candidate.
+winning_candidate = ''
 winning_count = 0
 winning_percentage = 0
 
-#Open the election results and read the file.
-with open(file_to_load) as election_data:  
+#Open the election results and read that file.
+with open(file_to_load) as election_data:
+    
+    #Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    #Read the header row
+    #Read the header row.
     headers = next(file_reader)
     
-    #Print each row in the CSV file
+    #Print each row in the CSV file.
     for row in file_reader:
-        #Add to the total vote count.
+        
+        #Add to the total vote count
         total_votes += 1
 
-        #Print the candidate name from each row
+        #Get the candidate name from each row.
         candidate_name = row[2]
 
-        #If the candidate does not match any existing candidate
+        #Check to see if the candidate_name is in the candidate_options list.
         if candidate_name not in candidate_options:
-            
-            #Add it to the list of candidates
+        
+            #Add the candidate name to the candidate list.
             candidate_options.append(candidate_name)
 
-            #Begin tracking that candidate's vote count
+            #Begin tracking that candidate's vote count.
             candidate_votes[candidate_name] = 0
 
-        #Adding vote
+        #Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
 
-#Determine the percentage of votes for reach candidate by looping through the counts.
+    #Loop through the candidate_votes dictionary for the candidate's name.
+    for candidate_name in candidate_votes:
+            
+        #Set the votes equal to the candidate's vote value of the candidate name key.
+        votes = candidate_votes[candidate_name]
 
-#Iterate through the candidate list
-for candidate_name in candidate_votes:
+        #Calculate the vote percentage and transform integers to float.
+        vote_percentage = float(votes)/float(total_votes) * 100
+       
+        #Print out each candidate's name, vote count and percentage of votes to the terminal.
+        print(f'{candidate_name}: {vote_percentage:1f}% ({votes:,})\n')
 
-    #Retrieve the vote count of a candidate
-    votes = candidate_votes[candidate_name]
-
-    #Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
-
-    #To do: print out each candidate's name, vote count, and percentage of votes to the terminal
-
-    print(f"{candidate_name}: {vote_percentage: 1f}% ({votes:,})\n")
-
-    #Determine winning vote and candidate
-    #Determine if the votes is greater than the winning count.
-    if (votes>winning_count) and (vote_percentage) > (winning_percentage):
-            #If true then set winning_count = votes and winning_percent = vote percentage
+        #Determine if the votes are greater than the winning count and the percentage is greater than the winning percentage.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            
+            #If both of these are true, set the votes, vote percentage and candidate to the values indicated. 
             winning_count = votes
             winning_percentage = vote_percentage
-            #And set the winning candidate euqal to the candidates name.
             winning_candidate = candidate_name
 
-#To do: print out the winning candidate, vote count and percentage to terminal.
-#votes to the terminal
+    winning_candidate_summary = (
+        f'---------------------------\n'
+        f'Winner: {winning_candidate}\n'
+        f'Winning Vote Count: {winning_count:,}\n'
+        f'Winning Percentage: {winning_percentage:.1f}%\n'
+        f'---------------------------\n')
     
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
+    print(winning_candidate_summary)
+
+#Print the candidate vote dictionary        
+print(candidate_votes)
--- Python_practice.py
+++ Python_practice.py
@@ -1,9 +1,39 @@
-import os
-import csv
-#cwd = os.getcwd()
-#print("My current working directory is: {} ".format(cwd))
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
 
-file_to_save = os.path.join('analysis','election_analysis.txt')
-with open(file_to_save, 'w') as txt_file:
-    txt_file.write('Hello World')
-    
+# temperature = int(input("What is the temperature outside?"))
+
+# if temperature > 80:
+#     print('Turn on the AC.')
+# else:
+#     print('Open the windows.')
+
+#What is the score?
+score = int(input("What is your test score?"))
+
+#Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# else:
+#     if score >= 80:
+#         print('Your grade is a B.')
+#     else:
+#         if score >= 70:
+#             print('Your grade is a C.')
+#         else:
+#             if score >= 60:
+#                 print('Your grade is a D.')
+#             else:
+#                 print('Your grade is an F.')
+
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
